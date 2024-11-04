6dd2eb105ac0a2e69868bf0b42e030e3c28854db
MFarhanFansuri <farhanfansurigithub@gamil.com>
2024-11-04 20:50:12 +0700

    hand on

--- main.go
+++ main.go
@@ -1,20 +1,51 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	x := 12
-	y := &x
+	// 1. buat object product
+	produk1 := Product{
+		Name:  "Sabun",
+		Price: 12500,
+		Stock: 12,
+	}
 
-	fmt.Println("nilai variabel x : ", x)
-	fmt.Println("nilai variabel y : ", *y)
+	var new_price float32 = 10500
 
-	fmt.Println("nilai pointer x : ", &x)
-	fmt.Println("nilai pointer y : ", y)
+	// 2. update price
+	produk1.updatePrice(&new_price)
 
-	x = 2
-	fmt.Println("nilai variabel x : ", x)
-	fmt.Println("nilai variabel y : ", *y)
+	// 3. cetak price
+	fmt.Println(produk1.Price)
+
+	// 4. update stok
+	var new_stock uint = 20
+	produk1.updateStock(&new_stock)
+
+	// 5. Tampilkan nilai stock yang sekarang
+	fmt.Println(produk1.Stock)
+}
+
+type student struct {
+	name  string
+	grade int
+}
+
+type Product struct {
+	Name  string
+	Price float32
+	Stock uint
+}
+
+func (p *Product) updatePrice(harga *float32) {
+	p.Price = *harga
+}
+
+func (p *Product) updateStock(stok *uint) {
+	p.Stock = *stok
+}
+
+type class struct {
+	kelas   string
+	student student
 }
